d5f268fa318cdb0511d651654d3229e5bcbe7ce9
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-02-12 13:24:57 +0600

    Completed: Find the Runner-Up Score!

--- Language Proficiency (Python)/Find_the_Runner_Up_Score.py
+++ Language Proficiency (Python)/Find_the_Runner_Up_Score.py
@@ -1,9 +1,5 @@
-# Incomplete
-# Gets WA on 2 cases
-
 if __name__ == '__main__':
     n = int(input())
     arr = list(set(map(int, input().split())))
-
-    sorted(arr)
-    print(arr[len(arr) - 2])
+    arr.sort()
+    print(arr[-2] if len(arr) > 1 else arr[0])
