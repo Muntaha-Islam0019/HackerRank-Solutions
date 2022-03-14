54f5c02de5ed77c40d31a5bbfcc75d0bc5a36c8a
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-03-14 18:52:10 +0600

    Solved: Jim and the Orders

---
+++ Problem Solving/Jim and the Orders.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jimOrders' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts 2D_INTEGER_ARRAY orders as parameter.
+#
+
+def jimOrders(orders):
+    # Write your code here
+
+    s = sorted(enumerate(orders,1),key=lambda x:sum(x[1]))
+    return (i[0] for i in s)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    orders = []
+
+    for _ in range(n):
+        orders.append(list(map(int, input().rstrip().split())))
+
+    result = jimOrders(orders)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
